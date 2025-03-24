5d325a0784374ab5d6eeefcecca8a6d752e463bb
JajouDev <jahelgithub64@gmail.com>
2025-03-24 15:33:52 +0100

    Devfinder

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,15 @@
+const $body = document.querySelector('body')
 const $form = document.querySelector("form");
 const $inputSearch = document.querySelector(".input-search-username");
+const $darkMode = document.querySelector('.dark-mode')
+
+$darkMode.addEventListener('click', () => {
+  if(document.body.classList.contains('dark-mode')) {
+    document.body.classList.remove('dark-mode')
+  } else {
+    document.body.classList.add('dark-mode')
+  }
+})  
 
 function updatedDataUsername(githubData) {
   const $userPicture = document.querySelector(".user-profile");
@@ -9,14 +19,14 @@ function updatedDataUsername(githubData) {
   const $reposNumber = document.querySelector(".repos-number");
   const $followersNumber = document.querySelector(".followers-number");
   const $followingNumber = document.querySelector(".following-number");
-  const $usernameLocation = document.querySelector(".username-location");
-  const $usernameLink = document.querySelector(".username-link");
-  const $usernameSocial = document.querySelector(".username-social");
-  const $usernameEnterprise = document.querySelector(".username-enterprise");
+  const $usernameLocation = document.querySelector(".user-location");
+  const $usernameLink = document.querySelector(".user-link");
+  const $usernameSocial = document.querySelector(".user-social");
+  const $usernameEnterprise = document.querySelector(".user-enterprise");
 
   $userPicture.src = githubData.avatar_url;
   $usernameMain.textContent = githubData.name;
-  $usernameLogin.textContent = githubData.login;
+  $usernameLogin.textContent = "@" + githubData.login;
   $usernameBio.textContent = githubData.bio;
   $reposNumber.textContent = githubData.public_repos;
   $followersNumber.textContent = githubData.followers;
@@ -29,7 +39,17 @@ function updatedDataUsername(githubData) {
     $usernameSocial.textContent = githubData.twitter_username;
   }
 
-  $usernameEnterprise.textContent = githubData.company;
+  if (githubData.company) {
+    $usernameEnterprise.textContent = githubData.company;
+  }
+}
+
+function resetData() {
+  if ($inputSearch.value === "") {
+    $usernameLogin.textContent = "@nofind"
+    // $userPicture.textContent = "Il n'y a pas de photo de profil";
+    $usernameBio.textContent = "This profile has no bio";
+  }
 }
 
 async function getUserInfo(searchedUser) {
@@ -40,7 +60,7 @@ async function getUserInfo(searchedUser) {
   });
 
   const userData = await res.json();
-
+  console.log(userData);
   updatedDataUsername(userData);
 }
 
@@ -49,4 +69,5 @@ $form.addEventListener("submit", (event) => {
 
   let searchValue = $inputSearch.value.trim();
   getUserInfo(searchValue);
+  resetData();
 });
